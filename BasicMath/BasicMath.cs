bbcb3865b517a6fc9f5a5c99a1dcb0932a07a6c5
AlexR4000 <Alexander.raagas@edu.sait.ca>
2025-03-21 19:12:50 -0600

    Add files via upload

---
+++ BasicMath/BasicMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLibrary
+{
+    public class BasicMath
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            return a / b;
+        }
+    }
+}
+
